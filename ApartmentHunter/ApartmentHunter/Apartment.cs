34a7405a6fd6763ad156a047e576244b92426961
Kyle Lin <kylelin47@gmail.com>
2014-07-18 15:21:21 -0400

    Started on Apartment class

---
+++ ApartmentHunter/ApartmentHunter/Apartment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ApartmentHunter
+{
+	public class Apartment
+	{
+		int[] attributes;
+
+		public Apartment (int[] attributes)
+		{
+			/*price, mile from campus*/
+			this.attributes = attributes;
+		}
+
+		public void setPrice(int newPrice)
+		{
+			attributes [0] = newPrice;
+		}
+
+		public void setMiles(int newMiles)
+		{
+			attributes [1] = newMiles;
+		}
+	}
+}
